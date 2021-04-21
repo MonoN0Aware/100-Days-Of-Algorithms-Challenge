8bb40219e98783ecdc2cf1f88cee6dc7200055fd
Boluwatife Adesina <badesina82@gmail.com>
2021-04-21 22:34:28 +0100

    21/4/2021 Algorithm

--- 100 days of algorithms.playground/Contents.swift
+++ 100 days of algorithms.playground/Contents.swift
@@ -25,38 +25,29 @@
 //}
 
 
-//func repeatStr(_ n: Int, _ string: String) -> String {
-//    if string == ""{
-//        return ""
-//    }else if n == 0{
-//       return ""
-//    }
-//
-// else{
-//        var strArr = Array(string)
-//        var count = 0
-//
-//        while count<n-1{
-//            //strArr.remove(at: 1)
-//            strArr.append(contentsOf: string)
-//            count+=1
-//
-//        }
-//
-//        return  String(strArr)
-//
-//    }
-//}
-//repeatStr(10, "ay")
-//
+func repeatStr(_ n: Int, _ string: String) -> String {
+    if string == ""{
+        return ""
+    }else if n == 0{
+       return ""
+    }
 
+ else{
+        var strArr = Array(string)
+        var count = 0
 
-//while count<n {
-//    str.append(i)
-//    count+=1
-//}
+        while count<n-1{
+            //strArr.remove(at: 1)
+            strArr.append(contentsOf: string)
+            count+=1
 
+        }
 
+        return  String(strArr)
+
+    }
+}
+repeatStr(10, "ay")
 
 //persistence(for: 39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
 //                       // and 4 has only one digit
@@ -65,6 +56,36 @@
 //                        // 1*2*6 = 12, and finally 1*2 = 2
 //
 // persistence(for: 4) === 0 // because 4 is already a one-digit number
+
+
+var count = 0
+func persistence(for num: Int) -> Int {
+    if num < 10{
+        return 0
+    }
+    
+    else{
+        let numtoArr = String(num)
+        let numToArrDigits = numtoArr.compactMap{$0.wholeNumberValue}
+        let total = numToArrDigits.reduce(1, *)
+       // print(total)
+        if total >= 10{
+            persistence(for:total)
+        }
+    }
+    count+=1
+   // print(count)
+    return count
+}
+
+
+
+
+persistence(for:3999)
+
+
+
+
 
 
 
