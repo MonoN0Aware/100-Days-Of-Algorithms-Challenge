9c674408f6705cd15e3d9fd3d0e8be470912f229
Boluwatife Adesina <badesina82@gmail.com>
2021-04-22 22:33:34 +0100

    22 4 21 PT 1

--- 100 days of algorithms.playground/Contents.swift
+++ 100 days of algorithms.playground/Contents.swift
@@ -1,4 +1,4 @@
-//
+import Foundation
 ////inArray
 //function inArray(array1,array2){
 //  var x;
@@ -26,68 +26,88 @@
 
 //21 APRIL ALGORITHM START
 
-func repeatStr(_ n: Int, _ string: String) -> String {
-    if string == ""{
-        return ""
-    }else if n == 0{
-       return ""
-    }
-
- else{
-        var strArr = Array(string)
-        var count = 0
-
-        while count<n-1{
-            //strArr.remove(at: 1)
-            strArr.append(contentsOf: string)
-            count+=1
-
-        }
-
-        return  String(strArr)
-
-    }
-}
-repeatStr(10, "ay")
-
-//persistence(for: 39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
-//                       // and 4 has only one digit
+//func repeatStr(_ n: Int, _ string: String) -> String {
+//    if string == ""{
+//        return ""
+//    }else if n == 0{
+//       return ""
+//    }
 //
-// persistence(for: 999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
-//                        // 1*2*6 = 12, and finally 1*2 = 2
+// else{
+//        var strArr = Array(string)
+//        var count = 0
 //
-// persistence(for: 4) === 0 // because 4 is already a one-digit number
-
-
-var count = 0
-func persistence(for num: Int) -> Int {
-    if num < 10{
-        return 0
-    }
-    
-    else{
-        let numtoArr = String(num)
-        let numToArrDigits = numtoArr.compactMap{$0.wholeNumberValue}
-        let total = numToArrDigits.reduce(1, *)
-       // print(total)
-        if total >= 10{
-            persistence(for:total)
-            
-        }
-    }
-    count+=1
-   // print(count)
-    return count
-}
-
-
-
-persistence(for:277777788888899)
-
+//        while count<n-1{
+//            //strArr.remove(at: 1)
+//            strArr.append(contentsOf: string)
+//            count+=1
+//
+//        }
+//
+//        return  String(strArr)
+//
+//    }
+//}
+//repeatStr(10, "ay")
+//
+////persistence(for: 39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4=4
+////                       // and 4 has only one digit
+////
+//// persistence(for: 999) === 4 // because 9*9*9 = 729, 7*2*9 = 126,
+////                        // 1*2*6 = 12, and finally 1*2 = 2
+////
+//// persistence(for: 4) === 0 // because 4 is already a one-digit number
+//
+//
+//var count = 0
+//func persistence(for num: Int) -> Int {
+//    if num < 10{
+//        return 0
+//    }
+//
+//    else{
+//        let numtoArr = String(num)
+//        let numToArrDigits = numtoArr.compactMap{$0.wholeNumberValue}
+//        let total = numToArrDigits.reduce(1, *)
+//       // print(total)
+//        if total >= 10{
+//            persistence(for:total)
+//
+//        }
+//    }
+//    count+=1
+//   // print(count)
+//    return count
+//}
+//
+//
+//
+//persistence(for:277777788888899)
+//
 
 
 //21 APRIL ALGORITHM END
 
+//22 APRIL ALGORITH START
 
 
+
+
+func nbDig(_ n: Int, _ d: Int) -> Int {
+    var nArray:[Int] = []
+    var count = 0
+    for i in 0...n{
+        let x = i*i
+        nArray.append(x)
+    }
+    for j in nArray {
+        for l in "\(j)" {
+            if String(l).contains("\(d)") {
+                count += 1
+            }
+        }
+    }
+    return count
+}
+nbDig(8, 5)
 
